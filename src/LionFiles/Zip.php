f1c76a7089d9d1bd367910d73a417a9c2299a46b
Sleon4 <sergioleon4004@hotmail.com>
2024-06-29 16:54:57 -0500

    docs: annotations have been added to document classes

--- src/LionFiles/Zip.php
+++ src/LionFiles/Zip.php
@@ -7,70 +7,122 @@ namespace Lion\Files;
 use Lion\Files\Store;
 use ZipArchive;
 
+/**
+ * Manage files to compress or decompress in ZIP format
+ *
+ * @property Store $store [Manipulate system files]
+ * @property ZipArchive $zipArchive [A file archive, compressed with Zip]
+ * @property array<int, string> $deleteFiles [List of files that are deleted in
+ * management]
+ *
+ * @package Lion\Files
+ */
 class Zip
 {
+    /**
+     * [Manipulate system files]
+     *
+     * @var Store $store
+     */
     private Store $store;
-	private ZipArchive $zipArchive;
 
-	private array $deleteFiles = [];
+    /**
+     * [A file archive, compressed with Zip]
+     *
+     * @var ZipArchive $zipArchive
+     */
+    private ZipArchive $zipArchive;
+
+    /**
+     * [List of files that are deleted in management]
+     *
+     * @var array<int, string> $deleteFiles
+     */
+    private array $deleteFiles = [];
 
     public function __construct()
     {
         $this->store = new Store();
+
         $this->zipArchive = new ZipArchive();
     }
 
     /**
      * Unzip a ZIP file from a defined path to a defined path
-     * */
-	public function decompress(string $from, string $to): void
+     *
+     * @param string $from [Path from where files are obtained]
+     * @param string $to [Path where files are stored]
+     *
+     * @return void
+     */
+    public function decompress(string $from, string $to): void
     {
         $this->zipArchive->open($this->store->normalizePath($from));
+
         $this->zipArchive->extractTo($this->store->normalizePath($to));
+
         $this->zipArchive->close();
-	}
+    }
 
     /**
      * Create a ZIP file at a defined path
-     * */
-	public function create(string $zipName): Zip
+     *
+     * @param string $zipName [ZIP file name]
+     *
+     * @return Zip
+     */
+    public function create(string $zipName): Zip
     {
-		$this->zipArchive->open($this->store->normalizePath($zipName), ZipArchive::CREATE);
+        $this->zipArchive->open($this->store->normalizePath($zipName), ZipArchive::CREATE);
 
         return $this;
-	}
+    }
 
     /**
      * Add files from a defined path to the ZIP archive
-     * */
-	public function add(array $files): Zip
+     *
+     * @param array<int, string> $files [List of files to be compressed]
+     *
+     * @return Zip
+     */
+    public function add(array $files): Zip
     {
-		foreach ($files as $file) {
-			$this->zipArchive->addFile($file, $this->store->getBasename($file));
-		}
+        foreach ($files as $file) {
+            $this->zipArchive->addFile($file, $this->store->getBasename($file));
+        }
 
         return $this;
-	}
+    }
 
     /**
      * Add files sent through a request to the ZIP archive
-     * */
-	public function addUpload(string $path, string $file, string $fileName): Zip
+     *
+     * @param string $path [Defined route]
+     * @param string $tmpName [Temporary file]
+     * @param string $name [File name]
+     *
+     * @return Zip
+     */
+    public function addUpload(string $path, string $tmpName, string $name): Zip
     {
-		$this->store->upload($file, $fileName, $path);
+        $this->store->upload($tmpName, $name, $path);
 
-        $filePath = $this->store->normalizePath($path . $fileName);
+        $filePath = $this->store->normalizePath($path . $name);
 
-		$this->zipArchive->addFile($filePath, $this->store->getBasename($fileName));
+        $this->zipArchive->addFile($filePath, $this->store->getBasename($name));
 
-		array_push($this->deleteFiles, $filePath);
+        array_push($this->deleteFiles, $filePath);
 
         return $this;
-	}
+    }
 
     /**
      * Save the ZIP file with the current data
-     * */
+     *
+     * @return void
+     *
+     * @throws Exception [If an error occurs while deleting the file]
+     */
     public function save(): void
     {
         $this->zipArchive->close();
